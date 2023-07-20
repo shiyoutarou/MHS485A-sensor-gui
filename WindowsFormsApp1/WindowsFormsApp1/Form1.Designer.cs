7b82601d34b007c8cf98938eec56341c6a270e72
touhousanae <saigyoujiyuyukoth@gmail.com>
2023-07-20 18:42:03 +0800

    updated

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,36 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button2 = new System.Windows.Forms.Button();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button2
             // 
+            this.button2.Font = new System.Drawing.Font("新細明體", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.button2.Location = new System.Drawing.Point(125, 192);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(157, 62);
             this.button2.TabIndex = 0;
-            this.button2.Text = "button2";
+            this.button2.Text = "連線";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(125, 291);
+            this.richTextBox2.Location = new System.Drawing.Point(92, 271);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(287, 96);
+            this.richTextBox2.Size = new System.Drawing.Size(620, 130);
             this.richTextBox2.TabIndex = 1;
             this.richTextBox2.Text = "";
             this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(125, 153);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.ClientSize = new System.Drawing.Size(789, 512);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.button2);
             this.Name = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +80,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WindowsFormsApp1
 {
@@ -19,10 +21,48 @@ namespace WindowsFormsApp1
 
         private void button2_Click(object sender, EventArgs e)
         {
-            richTextBox2.Text = "Hello World";
+            this.serialPort1.Close();                       //關閉COM連線
+            if (serialPort1.IsOpen == false)
+            {
+                this.serialPort1.PortName = textBox1.Text; // 設定使用的 PORT
+                if (!serialPort1.IsOpen)                   // 檢查 PORT 是否關閉
+                    this.serialPort1.Close();
+                this.serialPort1.BaudRate = 9600;            // baud rate = 9600
+                this.serialPort1.Parity = Parity.None;       // Parity = none
+                this.serialPort1.StopBits = StopBits.One;    // stop bits = one
+                this.serialPort1.DataBits = 8;               // data bits = 8                
+                this.serialPort1.ReadTimeout = 2000;
+                this.serialPort1.Encoding = Encoding.Default;
+                try
+                {
+                    serialPort1.Open(); // 打開 PORT
+                    if (serialPort1.IsOpen == true)
+                    {
+                        richTextBox2.Text += "連線成功\r\n";
+                    }
+                    else
+                    {
+                        richTextBox1.Text = "NOT CONNECTED\r\n";
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Error: " + "連線失敗", "ERROR");
+                }
+            }
+            else
+            {
+                this.serialPort1.Close();// 關閉 PORT
+                richTextBox1.Text = "離開連線成功";
+            }
         }
 
         private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
