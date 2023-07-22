6b94d76048eff73f133db8f3bb690e2be0436e20
touhousanae <saigyoujiyuyukoth@gmail.com>
2023-07-22 12:44:48 +0800

    updated

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -97,7 +97,7 @@ namespace WindowsFormsApp1
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            
         }
 
         private void button3_Click(object sender, EventArgs e)
