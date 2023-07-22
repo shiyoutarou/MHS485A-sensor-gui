aa95ac1a175930bb9feac046002ed2a7559a0bd1
touhousanae <saigyoujiyuyukoth@gmail.com>
2023-07-22 14:45:05 +0800

    updated

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -36,12 +36,13 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button1.Location = new System.Drawing.Point(95, 76);
+            this.button1.Location = new System.Drawing.Point(44, 265);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(128, 52);
             this.button1.TabIndex = 0;
@@ -60,9 +61,9 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button4.Location = new System.Drawing.Point(95, 248);
+            this.button4.Location = new System.Drawing.Point(488, 265);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(128, 58);
+            this.button4.Size = new System.Drawing.Size(128, 52);
             this.button4.TabIndex = 3;
             this.button4.Text = "timer關閉";
             this.button4.UseVisualStyleBackColor = true;
@@ -71,7 +72,7 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button3.Location = new System.Drawing.Point(95, 190);
+            this.button3.Location = new System.Drawing.Point(337, 265);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(128, 52);
             this.button3.TabIndex = 2;
@@ -82,7 +83,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button2.Location = new System.Drawing.Point(95, 134);
+            this.button2.Location = new System.Drawing.Point(193, 265);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(128, 52);
             this.button2.TabIndex = 1;
@@ -92,16 +93,28 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(95, 48);
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(44, 223);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.Size = new System.Drawing.Size(100, 27);
             this.textBox1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(198, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 27);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button4);
@@ -124,6 +137,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
