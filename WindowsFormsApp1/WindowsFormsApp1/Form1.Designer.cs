6870a5b4fe75ad64db14395fca571246ca89967d
touhousanae <saigyoujiyuyukoth@gmail.com>
2023-07-24 00:20:19 +0800

    updated

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -51,9 +51,9 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button1.Location = new System.Drawing.Point(40, 174);
+            this.button1.Location = new System.Drawing.Point(12, 174);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 52);
+            this.button1.Size = new System.Drawing.Size(116, 44);
             this.button1.TabIndex = 0;
             this.button1.Text = "連線";
             this.button1.UseVisualStyleBackColor = true;
@@ -61,18 +61,18 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(44, 374);
+            this.richTextBox1.Location = new System.Drawing.Point(12, 377);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(720, 56);
+            this.richTextBox1.Size = new System.Drawing.Size(740, 112);
             this.richTextBox1.TabIndex = 5;
             this.richTextBox1.Text = "";
             // 
             // button4
             // 
-            this.button4.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button4.Location = new System.Drawing.Point(189, 241);
+            this.button4.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.Location = new System.Drawing.Point(146, 224);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(128, 52);
+            this.button4.Size = new System.Drawing.Size(114, 45);
             this.button4.TabIndex = 3;
             this.button4.Text = "timer關閉";
             this.button4.UseVisualStyleBackColor = true;
@@ -80,10 +80,10 @@
             // 
             // button3
             // 
-            this.button3.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button3.Location = new System.Drawing.Point(40, 241);
+            this.button3.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(12, 224);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(128, 52);
+            this.button3.Size = new System.Drawing.Size(116, 45);
             this.button3.TabIndex = 2;
             this.button3.Text = "timer開啟";
             this.button3.UseVisualStyleBackColor = true;
@@ -92,9 +92,9 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button2.Location = new System.Drawing.Point(189, 174);
+            this.button2.Location = new System.Drawing.Point(146, 174);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(128, 52);
+            this.button2.Size = new System.Drawing.Size(114, 44);
             this.button2.TabIndex = 1;
             this.button2.Text = "讀取";
             this.button2.UseVisualStyleBackColor = true;
@@ -103,7 +103,7 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox1.Location = new System.Drawing.Point(40, 136);
+            this.textBox1.Location = new System.Drawing.Point(12, 136);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 27);
             this.textBox1.TabIndex = 6;
@@ -112,7 +112,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(184, 136);
+            this.label1.Location = new System.Drawing.Point(152, 136);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(74, 27);
             this.label1.TabIndex = 7;
@@ -130,7 +130,7 @@
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(337, 12);
+            this.chart1.Location = new System.Drawing.Point(344, 12);
             this.chart1.Name = "chart1";
             this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.EarthTones;
             series1.ChartArea = "ChartArea1";
@@ -138,7 +138,7 @@
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(417, 281);
+            this.chart1.Size = new System.Drawing.Size(396, 279);
             this.chart1.TabIndex = 8;
             this.chart1.Text = "chart1";
             this.chart1.Click += new System.EventHandler(this.chart1_Click);
@@ -149,7 +149,7 @@
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(44, 308);
+            this.richTextBox2.Location = new System.Drawing.Point(12, 297);
             this.richTextBox2.Name = "richTextBox2";
             this.richTextBox2.Size = new System.Drawing.Size(273, 60);
             this.richTextBox2.TabIndex = 9;
@@ -159,7 +159,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1104, 581);
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.label1);
